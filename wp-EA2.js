712907fa1b07ceb781c7a117a0f9f387761cc1af
michaelbl72 <michael.blum@stud.fh-luebeck.de>
2015-10-28 20:12:56 +0100

    Fertigstellung EA2

--- wp-EA2.js
+++ wp-EA2.js
@@ -8,15 +8,27 @@ function addListenersFirst(){
 	// b) use variable to add event listener
 	var rechteBox = document.getElementsByClassName("rechtebox");
 	rechteBox[0].addEventListener('mouseover', flipMe);
-	
 	// Add mouseover listener to the upper div with classname 'links'
 	var linkeBox = document.getElementsByClassName("linkebox");
 	linkeBox[0].addEventListener('click', copyMe);	
 
 
 	// Add listeners to action elements for "Make Alerts" "Bezeichner" and "Schreibe den Text"
-
+	var makealerts = document.querySelector("#a2 input");
+	makealerts.addEventListener('click', makeAlerts);
+	
+	var bezeichner = document.querySelector("#switch");
+	bezeichner.addEventListener('click', switchMe);
+	
+	var text = document.querySelector("#createtext");
+	text.addEventListener('click', createText);
 	// add listener to form input elements
+	var farbe_links = document.getElementById('colorLeft');
+    farbe_links.addEventListener('change', changeColor);
+    var farbe_mitte = document.getElementById('colorMiddle');
+    farbe_mitte.addEventListener('change', changeColor);
+    var farbe_rechts = document.getElementById('colorRight');
+    farbe_rechts.addEventListener('change', changeColor);
 
 }
 
@@ -25,13 +37,14 @@ function addListenersFirst(){
 	copyMe copies the text from one element to another
 */
 function copyMe(){
+	//alert("funktion copyMe");
 	// put the "container"-element (to where the text should copied) into the 
 	// variable container (target element)
-	container = document.getElementsByID('container');
+	var container = document.getElementById('container');
 	// put the element to be read into the variable divlinks (source element)
-	divlinks = rechteBox[0];
-	// put the content of the source element into text node of the target element
-	container.innerHTML = rechteBox[0].innerHTML;
+	var linkebox = document.getElementsByClassName("linkebox");
+   	// put the content of the source element into text node of the target element
+	container.innerHTML = linkebox[0].innerHTML;    
 }
 
 /*
@@ -41,7 +54,10 @@ function copyMe(){
 function flipMe(){
 	// works similar to copyMe
 	// in addition, delete the text from the source element
-
+	var rechtebox = document.getElementsByClassName("rechtebox");
+	var container = document.getElementById('container2');
+   	container2.innerHTML = rechtebox[0].innerHTML;
+	rechtebox[0].innerHTML = "";
 }
 
 
@@ -49,10 +65,11 @@ function flipMe(){
 	makeAlerts puts alerts to several elements
 */
 function makeAlerts(){
-	myp = document.querySelectorAll("#a2 p");
-
+	var myp = document.querySelectorAll("#a2 p");
 	// put event listener for each selected tag
-	
+	myp[0].addEventListener('click', showNewAlert);
+	myp[1].addEventListener('click', showNewAlert);
+	myp[2].addEventListener('click', showNewAlert);
 }
 
 /*
@@ -61,15 +78,21 @@ function makeAlerts(){
 function showNewAlert(){
 	// An alert should be displayed with the text of the paragraph that evoked the function.
 	// Use "this" to refer to the actual object
-	
+	var text = this.innerHTML;
+	alert(text);
 }
 
 /*
 	switchME switches the text of two boxes
 */
 function switchMe(){
-
-
+	var links_v = document.getElementById('links').innerHTML;
+    var mitte_v = document.getElementById('mitte').innerHTML;
+    var links_n = document.getElementById('links');
+    var mitte_n = document.getElementById('mitte');
+    
+    links_n.innerHTML = mitte_v;
+    mitte_n.innerHTML = links_v;
 }
 
 /*
@@ -80,6 +103,13 @@ function changeColor(){
 	// Variable idName stores the ID of the elements that invoked this function
 	var idName = this.id;
 
+	if (idName === "colorLeft") {
+        links.className = this.value;
+    } else if (idName === "colorMiddle") {
+        mitte.className = this.value;
+    } else {
+        rechts.className = this.value;
+    }
 	// Use if else statement to distinguish which select box has been used.
 	// For each case: select the html element that correspond to the used select box.
 	// For this element set a new attribute class with the name that corresponds to the selected value.
@@ -93,7 +123,12 @@ function changeColor(){
 	createText concatenates text from the input fields
 */
 function createText(){
+ 	var name = document.getElementById('name').value;
+    var number = document.getElementById('number').value;
+    var thing = document.getElementById('thing').value;
+    var text = document.getElementById('loesung');
 
+    text.innerHTML = name + " hat " + number + " " + thing + ".";
 }
 
 
